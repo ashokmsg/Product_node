61c451edb4abbd7f0c5ca6fbf4dc5de1a174ecfc
ashok <erashokmuthu@gmail.com>
2018-12-02 23:38:39 +0530

    added update and delete api

--- Controller/product.controller.js
+++ Controller/product.controller.js
@@ -59,6 +59,7 @@ exports.addProduct = async (req, res) => {
           "published_date":req.body.published_date
             }
         });
+        
         res.end();
         logWriter(`add product-i-finished`);
     } catch (error) {
@@ -72,10 +73,45 @@ exports.addProduct = async (req, res) => {
 
 exports.updateProduct = async (req, res) => {
     let client = getDbConnection();
+    let elastic = getElasticConnection();
     try {
-        logWriter(`update product-i-started`);
-       //code for update product ---om
-        logWriter(`update product-i-finished`);
+            logWriter(`update product-i-started`);
+            //code for update product ---om
+            var product_ID = req.body.id;
+            let searchResult = await elastic.search
+            ({
+            "index": "product_details",
+            "type": "_doc",
+            "id": product_ID
+            });
+            if (searchResult.hits.total == 1) {
+            await elastic.update
+            ({
+            "index": "product_details",
+            "type": "_doc",
+            "id": req.body.id,
+            "body":
+            {
+            "author": req.body.autor,
+            "isbn": req.body.isbn,
+            "title": req.body.title,
+            "unit": req.body.unit,
+            "product_type": req.body.product_type,
+            "special_price": req.body.special_price,
+            "orginal_price": req.body.original_price,
+            "category": req.body.category,
+            "published_date": req.body.published_date,
+            "page_count": req.body.page_count,
+            "status": req.body.status,
+            "thumbnail_url": req.body.thumbnail_url
+            }
+            });
+            res.end();
+            logWriter(`update product-i-finished`);
+            }
+            else{
+            logWriter(`update product-i-Product doesn't exist`)
+            }
     } catch (error) {
         logWriter(`update product-e-${error.toString()}`);
         console.log(error.toString());
@@ -87,9 +123,30 @@ exports.updateProduct = async (req, res) => {
 
 exports.deleteProduct = async (req, res) => {
     let client = getDbConnection();
+    let elastic = getElasticConnection();
     try {
         logWriter(`delete product-i-started`);
         //code for delete product --sanjay
+        // if(validations.validateDeleteID(req.params.product_id)){
+        //     handleError(constants.response_code.internal_server_error, res, constants.error_messages.internal_server_error);
+        //     return;
+        // }
+        let queryParams = [req.params.product_id];
+        let result1 = await client.query(constants.query.soft_delete_query, queryParams);
+        console.log("here");
+        let result2 = await elastic.deleteByQuery({
+            "index":"majestiic_book_house",
+            "type":"doc",
+            "body":{
+                "query":{
+                    "term":{
+                        "product_id":req.params.product_id
+                    }
+                }
+            }
+        });
+        console.log(result2);
+        res.send({});
         logWriter(`delete product-i-finished`);
     } catch (error) {
         logWriter(`delete product-e-${error.toString()}`);
@@ -99,5 +156,3 @@ exports.deleteProduct = async (req, res) => {
         client.end();
     }
 }
-
-
