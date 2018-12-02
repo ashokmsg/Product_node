44a5b987b5b7e08eb33fc94a80cfcfbbd2d1b2c5
Om Prakash TD <omprakash.duraisamy@gmail.com>
2018-12-02 22:50:41 +0530

    update data first commit

--- Controller/product.controller.js
+++ Controller/product.controller.js
@@ -1,5 +1,5 @@
-const { getDbConnection , getElasticConnection } = require("../Utils/dbConnections");
-const { handleError ,logWriter} = require("../Utils/utilMethods");
+const { getDbConnection, getElasticConnection } = require("../Utils/dbConnections");
+const { handleError, logWriter } = require("../Utils/utilMethods");
 const constants = require("../Config/constants");
 const validations = require("../Utils/validations");
 const keys = require("../Config/keys");
@@ -17,8 +17,8 @@ exports.sampleSearch = async (req, res) => {
         let result = await elastic.search({
             "index": "kibana_sample_data_flights",
         });
-       res.send(result);
-        
+        res.send(result);
+
         logWriter(`sample search-i-finished`);
     } catch (error) {
         logWriter(`sample search-e-${error.toString()}`);
@@ -34,27 +34,27 @@ exports.addProduct = async (req, res) => {
     let elastic = getElasticConnection();
     try {
         logWriter(`add product-i-started`);
-        var f=req.body.id;
-        let result=await elastic.index
-                ({
-            "index":"product_details",
-            "type":"_doc",
-            "body":
-            {
-          "author" : req.body.autor,
-          "isbn" : req.body.isbn,
-          "title" : req.body.title,
-          "unit" : req.body.unit,
-          "product_type" : req.body.product_type,
-          "special_price" : req.body.special_price,
-          "orginal_price" : req.body.original_price,
-          "category" : req.body.category,
-          "published_date":req.body.published_date,
-          "page_count":req.body.page_count,
-          "status":req.body.status,
-          "thumbnail_url":req.body.thumbnail_url
-            }
-        });
+        var f = req.body.id;
+        let result = await elastic.index
+            ({
+                "index": "product_details",
+                "type": "_doc",
+                "body":
+                {
+                    "author": req.body.autor,
+                    "isbn": req.body.isbn,
+                    "title": req.body.title,
+                    "unit": req.body.unit,
+                    "product_type": req.body.product_type,
+                    "special_price": req.body.special_price,
+                    "orginal_price": req.body.original_price,
+                    "category": req.body.category,
+                    "published_date": req.body.published_date,
+                    "page_count": req.body.page_count,
+                    "status": req.body.status,
+                    "thumbnail_url": req.body.thumbnail_url
+                }
+            });
         res.end();
         logWriter(`add product-i-finished`);
     } catch (error) {
@@ -70,7 +70,42 @@ exports.updateProduct = async (req, res) => {
     let client = getDbConnection();
     try {
         logWriter(`update product-i-started`);
-       //code for update product ---om
+        //code for update product ---om
+        var product_ID = req.body.id;
+        let searchResult = await elastic.search
+            ({
+                "index": "product_details",
+                "type": "_doc",
+                "id": product_ID
+            });
+        if (searchResult.hits.total == 1) {
+            await elastic.index
+                ({
+                    "index": "product_details",
+                    "type": "_doc",
+                    "body":
+                    {
+                        "id": req.body.id,
+                        "author": req.body.autor,
+                        "isbn": req.body.isbn,
+                        "title": req.body.title,
+                        "unit": req.body.unit,
+                        "product_type": req.body.product_type,
+                        "special_price": req.body.special_price,
+                        "orginal_price": req.body.original_price,
+                        "category": req.body.category,
+                        "published_date": req.body.published_date,
+                        "page_count": req.body.page_count,
+                        "status": req.body.status,
+                        "thumbnail_url": req.body.thumbnail_url
+                    }
+                });
+                res.end();
+                logWriter(`update product-i-finished`);
+        }
+        else{
+            logWriter(`update product-i-Product doesn't exist`)
+        }
         logWriter(`update product-i-finished`);
     } catch (error) {
         logWriter(`update product-e-${error.toString()}`);
